2ef1ad5eaad261e1f59e8699b570fb6d082c9e2a
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-03-06 14:46:51 +0530

    Added welcome message screen

--- APP/src/WelcomeScreenn.java
+++ APP/src/WelcomeScreenn.java
@@ -7,7 +7,7 @@ Description:This class demonstrates a welcome page that welcomes the user ans sh
 @version:1.0
  */
 public class WelcomeScreenn {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("============================================================");
         System.out.println("Welcome To The Hotel Booking Management System");
         System.out.println("System Initialized Successfully");
