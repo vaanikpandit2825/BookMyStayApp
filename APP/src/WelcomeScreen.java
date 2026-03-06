f467cc679c47222c38495ef0aba5027c5c9eaca9
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-03-06 14:26:27 +0530

    added application entry and welcome message

---
+++ APP/src/WelcomeScreen.java
@@ -0,0 +1,8 @@
+public class WelcomeScreen {
+     public static void main(String args[]){
+         System.out.println("============================================================");
+         System.out.println("Welcome To The Hotel Booking Management System");
+         System.out.println("System Initialized Successfully");
+         System.out.println("============================================================");
+     }
+}
